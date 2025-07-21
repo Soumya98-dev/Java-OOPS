7e329d12257c21919df8782d6bb00b4243e08755
Soumyadeep Chatterjee <soumyadeepchatterjee@Soumyadeeps-MacBook-Air.local>
2025-07-21 04:05:48 -0400

    bank Branch

--- Boxing/BankingSystem/Branch.java
+++ Boxing/BankingSystem/Branch.java
@@ -1,5 +1,51 @@
 package Boxing.BankingSystem;
 
-public class Branch {
+import java.util.*;
 
+public class Branch {
+  private String name;
+  private ArrayList<Customer> customers;
+
+  public Branch(String initialName) {
+    this.name = initialName;
+    this.customers = new ArrayList<Customer>();
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public ArrayList<Customer> getCustomers() {
+    return this.customers;
+  }
+
+  public boolean newCustomer(String nameOfCustomer, double initialTransaction) {
+    if (findCustomer(nameOfCustomer) != null) {
+      return false;
+    }
+    customers.add(new Customer(nameOfCustomer, initialTransaction));
+    return true;
+  }
+
+  public boolean addCustomerTransaction(String nameOfCustomer, double transaction) {
+    Customer existingCustomer = findCustomer(nameOfCustomer);
+
+    if (existingCustomer != null) {
+      existingCustomer.addTransaction(transaction);
+      return true;
+    }
+
+    return false;
+  }
+
+  public Customer findCustomer(String nameOfCustomer) {
+    for (int i = 0; i < this.customers.size(); i++) {
+      Customer customerInList = this.customers.get(i);
+      if (customerInList.getName().equals(nameOfCustomer)) {
+        return customerInList;
+      }
+    }
+
+    return null;
+  }
 }
